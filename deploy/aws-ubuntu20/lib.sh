8f973090dad895632fdc08ed094e3fc5a4888ea6
Victor Garcia <102045600+vgarciaf@users.noreply.github.com>
2022-09-08 16:29:13 +0200

    before install fixes

--- deploy/aws-ubuntu20/lib.sh
+++ deploy/aws-ubuntu20/lib.sh
@@ -55,15 +55,10 @@ installPhp() {
     
     if [ ! -z $PHP_VERSION ]; then
         return
-    fi
-    
-    if [ -f /etc/apt/sources.list.d/ondrej-php-*.list ]; then
-        return
-    fi
-    
-    sudo apt install -y lsb-release ca-certificates apt-transport-https software-properties-common gnupg2
-    echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/sury-php.list
-    curl -fsSL  https://packages.sury.org/php/apt.gpg| sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/sury-keyring.gpg
+    fi    
+
+    sudo apt install lsb-release ca-certificates apt-transport-https software-properties-common -y
+    sudo add-apt-repository ppa:ondrej/php
     
     sudo apt -y update
     sudo apt -y install php${PHP_VERSION}
