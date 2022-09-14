ec83c8dac8bf0e4755f3ffe494a396e0a3ba2f21
Victor Garcia <102045600+vgarciaf@users.noreply.github.com>
2022-09-14 10:22:03 +0200

    fix aws client install

--- deploy/libs/aws_install.sh
+++ deploy/libs/aws_install.sh
@@ -177,9 +177,6 @@ generateSwapFile() {
 
 # Aws client
 installAwsClient() {
-    mkdir /tmp/aws
-    cd /tmp/aws
-
     curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
     unzip awscliv2.zip
     sudo ./aws/install
