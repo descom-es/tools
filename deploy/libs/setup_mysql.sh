dfdab24b0ed8bd031f6becc1398daec82f9a9e4e
Cesar <cesargb@gmail.com>
2022-10-07 12:32:42 +0200

    add setup mysql

---
+++ deploy/libs/setup_mysql.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+createDatabase() {
+    if [ -z "$1" ]; then
+        return false
+    fi
+
+    DATABASE=$1
+
+    mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${DATABASE};"
+
+    if [ $? -ne 0 ]; then
+        return false
+    fi
+
+    return true
+}
+
+createUser() {
+    if [ -z "$1" -o -z "$2" ]; then
+        return false
+    fi
+
+    USER=$1
+    PASSWORD=$2
+
+    mysql -u root -e "CREATE USER IF NOT EXISTS '${USER}'@'localhost' IDENTIFIED BY '${PASSWORD}';"
+    mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '${USER}'@'localhost';"
+    mysql -u root -e "FLUSH PRIVILEGES;"
+}
