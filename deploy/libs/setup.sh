b39ba1c7f80b6b167c9ad4b1a97a33e1f66c7040
Cesar <cesargb@gmail.com>
2022-10-07 13:09:12 +0200

    fix type return

--- deploy/libs/setup.sh
+++ deploy/libs/setup.sh
@@ -2,7 +2,7 @@
 
 mysqlCreateDatabase() {
     if [ -z "$1" ]; then
-        return false
+        return 0
     fi
 
     DATABASE=$1
@@ -10,15 +10,15 @@ mysqlCreateDatabase() {
     mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${DATABASE};"
 
     if [ $? -ne 0 ]; then
-        return false
+        return 0
     fi
 
-    return true
+    return 1
 }
 
 mysqlCreateUser() {
     if [ -z "$2" ]; then
-        return false
+        return 0
     fi
 
     USER=$1
@@ -27,4 +27,6 @@ mysqlCreateUser() {
     mysql -u root -e "CREATE USER IF NOT EXISTS '${USER}'@'localhost' IDENTIFIED BY '${PASSWORD}';"
     mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '${USER}'@'localhost';"
     mysql -u root -e "FLUSH PRIVILEGES;"
+
+    return 1
 }
