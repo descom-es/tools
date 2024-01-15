abf77d9f6a39327c5f7c1f9131097ab7e809f54b
Cesar Garcia <cesargb@gmail.com>
2024-01-15 14:50:55 +0100

    Update nvm node installer

--- deploy/libs/aws_install.sh
+++ deploy/libs/aws_install.sh
@@ -221,7 +221,7 @@ installNode() {
     command -v nvm > /dev/null
 
     if [ $? -ne 0 ]; then
-        wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+        wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
         export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
         [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
         
